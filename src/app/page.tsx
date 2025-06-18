9c405003fa71ec4c56387978a0a5d43bd02ae871
Satria Arya Diva <satriaarya125@gmail.com>
2025-06-18 07:38:41 +0700

    add :ssg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Hero from "./components/hero";
 import Navbar from "./components/nav";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="  items-center justify-items-center min-h-screen  w-full   font-[family-name:var(--font-geist-sans)]">
       <Navbar/>
